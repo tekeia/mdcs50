7fee9778b24e69ad9e059197cf8fd1c6f4f5bab9
tekeia <leonardo.borges003@mymdc.net>
2015-06-11 19:53:47 -0400

    Added more dynamic strings

--- web/bootstrap.php
+++ web/bootstrap.php
@@ -5,3 +5,10 @@
  * Date: 6/11/15
  * Time: 6:39 PM
  */
+
+$website_settings = [
+    'title' => 'Leonardo Borges',
+    'page_title' => 'O exemeplo de homem brasileiro.',
+    'copright' => 'Copyright &copy; Your Website',
+    'home' => 'Pirate'
+];
